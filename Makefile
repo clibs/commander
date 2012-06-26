e7e28ef4a3e1ef631acca432bd315bdeb41755e7
rhersen <rhersen@gmail.com>
2012-06-26 22:28:39 +0200

    added more unit tests

--- Makefile
+++ Makefile
@@ -1,12 +1,10 @@
-CC = gcc
-
 test: test.c src/commander.c
-	$(CC) -Wall $^ -std=c99 -o $@
+	$(CC) $^ -std=c99 -o $@
 
 unit: unit.c src/commander.c
-	$(CC) -Wall -g -fprofile-arcs -ftest-coverage $^ -std=c99 -o $@
+	$(CC) $^ -std=c99 -o $@
 
 clean:
-	rm -f test
+	rm -f test unit
 
 .PHONY: clean
--- unit.c
+++ unit.c
@@ -1,28 +1,30 @@
-#include <stdio.h>
-#include <sys/time.h>
 #include <setjmp.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
 
 #include "src/commander.h"
  
-#define assertEqualsI(a,e) _assertEqualsI(a,e,__FILE__,__LINE__)
+#define assertEquals(a,e) _assertEqualsI(a,e,__func__,__LINE__)
+#define assertEqualStr(a,e) _assertEqualsI(strcmp(a,e),0,__func__,__LINE__)
 
-static void _assertEqualsI(int actual, int expected, char *file, int line) {
+static void _assertEqualsI(int actual, int expected, const char *file, int line) {
     if (actual == expected) {
-        printf("%s:%d:\033[0;32m %d OK\033[0m\n", file, line, actual);
+        printf("%s/%d:\033[0;32m OK\033[0m\n", file, line);
     } else {
-        printf("%s:%d:\033[0;31m expected %d but got %d\033[0m\n",
+        printf("%s/%d:\033[0;31m expected %d but got %d\033[0m\n",
                file, line, expected, actual);
     }
 }
 
-#define assertFalse(a) _assertTrue(!(a),__FILE__,__LINE__)
-#define assertTrue(a) _assertTrue((a),__FILE__,__LINE__)
+#define assertFalse(a) _assertTrue(!(a),__func__,__LINE__)
+#define assertTrue(a) _assertTrue((a),__func__,__LINE__)
 
-static void _assertTrue(int actual, char *file, int line) {
+static void _assertTrue(int actual, const char *file, int line) {
     if (actual) {
-        printf("%s:%d:\033[0;32m %d OK\033[0m\n", file, line, actual);
+        printf("%s/%d:\033[0;32m OK\033[0m\n", file, line);
     } else {
-        printf("%s:%d:\033[0;31m got %d\033[0m\n", file, line, actual);
+        printf("%s/%d:\033[0;31m got %d\033[0m\n", file, line, actual);
     }
 }
 
@@ -37,6 +39,7 @@ static double getElapsedSecondsSince(struct timeval* start) {
 
 static int isVerboseSet = 0;
 static int isRequiredSet = 0;
+static int isOptionalSet = 0;
 static int exitStatus = 0;
 
 static void
@@ -47,6 +50,11 @@ verbose(command_t *self) {
 static void
 required(command_t *self) {
   isRequiredSet = 1;
+}
+
+static void
+optional(command_t *self) {
+  isOptionalSet = 1;
 }
  
 static jmp_buf exitCalled;
@@ -59,19 +67,13 @@ static void setStatus(int status) {
 static void setUp() {
   isVerboseSet = 0;
   isRequiredSet = 0;
+  isOptionalSet = 0;
   exitStatus = 0;
 }
 
 static void init(command_t* target) {
   command_init(target, "name", "0.0.1");
   target->exit = setStatus;
-}
-
-static void shouldDoNothingIfArgumentListIsEmpty(void) {
-  setUp();
-  command_t target = {0};
-  if (!setjmp(exitCalled)) command_parse(&target, 0, 0);
-  assertEqualsI(target.data, 0);
 }
 
 static void shouldEnableVerbose(void)
@@ -94,7 +96,7 @@ static void shouldSetExitStatus(void)
   char* argv[] = { "name", "-w", 0 };
   if (!setjmp(exitCalled)) command_parse(&target, 2, argv);
   assertFalse(isVerboseSet);
-  assertEqualsI(exitStatus, 1);
+  assertEquals(exitStatus, 1);
 }
 
 static void shouldSetRequired(void)
@@ -105,7 +107,7 @@ static void shouldSetRequired(void)
   command_option(&target, "-r", "--required <arg>", "required arg", required);
   char* argv[] = { "name", "-r", "val", 0 };
   if (!setjmp(exitCalled)) command_parse(&target, 3, argv);
-  assertEqualsI(exitStatus, 0);
+  assertEquals(exitStatus, 0);
   assertTrue(isRequiredSet);
 }
 
@@ -117,19 +119,70 @@ static void shouldFailIfRequiredIsMissingValue(void)
   command_option(&target, "-r", "--required <arg>", "required arg", required);
   char* argv[] = { "name", "-r", 0 };
   if (!setjmp(exitCalled)) command_parse(&target, 2, argv);
-  assertEqualsI(exitStatus, 1);
+  assertEquals(exitStatus, 1);
   assertFalse(isRequiredSet);
+}
+
+static void shouldSetOptional(void)
+{
+  setUp();
+  command_t target;
+  init(&target);
+  command_option(&target, "-o", "--optional [arg]", "optional arg", optional);
+  char* argv[] = { "name", "-o", "val", 0 };
+  if (!setjmp(exitCalled)) command_parse(&target, 3, argv);
+  assertEquals(exitStatus, 0);
+  assertTrue(isOptionalSet);
+}
+
+static void shouldNotFailIfOptionalIsMissingValue(void)
+{
+  setUp();
+  command_t target;
+  init(&target);
+  command_option(&target, "-o", "--optional [arg]", "optional arg", optional);
+  char* argv[] = { "name", "-o", 0 };
+  if (!setjmp(exitCalled)) command_parse(&target, 2, argv);
+  assertEquals(exitStatus, 0);
+  assertTrue(isOptionalSet);
+}
+
+static void shouldSetAdditional(void)
+{
+  setUp();
+  command_t target;
+  init(&target);
+  char* argv[] = { "name", "o", 0 };
+  if (!setjmp(exitCalled)) command_parse(&target, 2, argv);
+  assertEquals(exitStatus, 0);
+  assertEquals(target.argc, 1);
+  assertEqualStr(target.argv[0], "o");
+}
+
+static void shouldSetLiteral(void)
+{
+  setUp();
+  command_t target;
+  init(&target);
+  char* argv[] = { "name", "--", "-o", 0 };
+  if (!setjmp(exitCalled)) command_parse(&target, 3, argv);
+  assertEquals(exitStatus, 0);
+  assertEquals(target.argc, 1);
+  assertEqualStr(target.argv[0], "-o");
 }
 
 int main(void) {
     struct timeval start;
     gettimeofday(&start, 0);
 
-    shouldDoNothingIfArgumentListIsEmpty();
     shouldEnableVerbose();
     shouldSetExitStatus();
     shouldSetRequired();
     shouldFailIfRequiredIsMissingValue();
+    shouldSetOptional();
+    shouldNotFailIfOptionalIsMissingValue();
+    shouldSetAdditional();
+    shouldSetLiteral();
     printf("%f seconds\n", getElapsedSecondsSince(&start));
 
     return 0;

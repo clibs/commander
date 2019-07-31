f877b0a2083d60ee12f1b7d9daf471168365b44f
Bruno Dias <dias.h.bruno@gmail.com>
2019-07-31 18:23:31 +0000

    feature: omit short version of an option.

--- test.c
+++ test.c
@@ -17,6 +17,11 @@ optional(command_t *self) {
   printf("optional: %s\n", self->arg);
 }
 
+static void
+omit_short(command_t *self) {
+  printf("omitted short\n");
+}
+
 int
 main(int argc, char **argv){
   command_t cmd;
@@ -24,6 +29,7 @@ main(int argc, char **argv){
   command_option(&cmd, "-v", "--verbose", "enable verbose stuff", verbose);
   command_option(&cmd, "-r", "--required <arg>", "required arg", required);
   command_option(&cmd, "-o", "--optional [arg]", "optional arg", optional);
+  command_option(&cmd, NULL, "--omit-short", "omit short version by passing NULL", omit_short);
   command_parse(&cmd, argc, argv);
   printf("additional args:\n");
   for (int i = 0; i < cmd.argc; ++i) {
